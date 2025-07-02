8cf5d4776f92ec177d55039d76d38d139ef0798a
Guilherme Santos <86435735+guilhermesantos0@users.noreply.github.com>
2025-07-02 15:01:50 -0300

    chore: final adjusts

--- estagio-econverce/src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '*.svg' {
-    import * as React from 'react';
-    const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-    export { ReactComponent };
-}
